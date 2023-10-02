87e0142081f034fcbed6dc114f3e05fcda4633cd
bejeweled13 <134629734+bejeweled13@users.noreply.github.com>
2023-10-01 21:49:26 -0600

    Panel del círculo (#3)
    * Panel del círculo
    
    * Actualizada anotación Override
    
    * Panel del círculo actualizado
    
    * Eliminado .idea/misc.xml
    
    Debido a la diferencia de JDK se elimina .idea/misc.xml sólo para aceptar el PR
    
    * Creado misc.xml
    
    Creado misc.xml con correspondiente a proyecto principal
    
    ---------
    
    Co-authored-by: Uri Martínez <39018208+UriMtzF@users.noreply.github.com>

---
+++ src/mx/uaemex/fi/paradigmas_I/figuras/vista/PanelCirculoData.java
@@ -0,0 +1,35 @@
+package mx.uaemex.fi.paradigmas_I.figuras.vista;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class PanelCirculoData extends PanelDeFigura{
+
+    private JSpinner radio;
+
+    public PanelCirculoData(){
+
+        JLabel etqRadio;
+        SpinnerModel modelo;
+
+        setLayout(new GridLayout(1,2));
+
+        modelo = new SpinnerNumberModel(0,0,1000,1);
+
+        etqRadio = new JLabel("Radio: ");
+        radio= new JSpinner();
+        radio.setModel(modelo);
+
+        add(etqRadio);
+        add(radio);
+
+    }
+
+    @Override
+    public void setData() {
+
+            }
+        }
+
+
