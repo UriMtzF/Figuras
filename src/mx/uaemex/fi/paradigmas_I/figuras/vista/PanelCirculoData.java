09c2e9ff7b3214256853166c2e5262ebcb822fbe
Fernanda G M <fergarci0103@gmail.com>
2023-10-01 21:19:02 -0600

    Panel del círculo actualizado

--- src/mx/uaemex/fi/paradigmas_I/figuras/vista/PanelCirculoData.java
+++ src/mx/uaemex/fi/paradigmas_I/figuras/vista/PanelCirculoData.java
@@ -1,7 +1,5 @@
 package mx.uaemex.fi.paradigmas_I.figuras.vista;
 
-import mx.uaemex.fi.paradigmas_I.figuras.data.Figura;
-
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
@@ -11,28 +9,20 @@ public class PanelCirculoData extends PanelDeFigura{
     private JSpinner radio;
 
     public PanelCirculoData(){
-        super();
 
         JLabel etqRadio;
         SpinnerModel modelo;
-        JButton btnGD;
 
-        setLayout(new GridLayout(3,1));
+        setLayout(new GridLayout(1,2));
 
         modelo = new SpinnerNumberModel(0,0,1000,1);
 
         etqRadio = new JLabel("Radio: ");
         radio= new JSpinner();
         radio.setModel(modelo);
-        btnGD = new JButton("Guarda datos");
 
         add(etqRadio);
         add(radio);
-        add(btnGD);
-
-        setBorder(new EmptyBorder(10,10,10,10));
-
-
 
     }
 
